22f3277bf3f5234a3afad02934437a75637c4748
Alex Cheng <alexcheng94@126.com>
2018-09-14 13:18:53 +0800

    added comment explanation

--- src/components/disqus.js
+++ src/components/disqus.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { DiscussionEmbed } from 'disqus-react';
+import { DiscussionEmbed } from "disqus-react";
 
 export default class DisqueComment extends React.Component {
   render() {
-    const disqusShortname = "alexreactblog"
+    const disqusShortname = "alexreactblog";
     const disqusConfig = {
       url: window.location.href,
       identifier: this.props.id,
       title: this.props.title
-    };    
-    
+    };
+
     return (
       <div className="comments">
-        <div className='divider' style={{marginTop: '120px'}}></div>
-        <DiscussionEmbed
-          shortname={disqusShortname}
-          config={disqusConfig}
-        />
+        <div className="divider" style={{ marginTop: "120px" }} />
+        <small>
+          Comments powered by Disqus (需梯子访问)
+          <span role="img" aria-label="smilling emoji">
+            😀
+          </span>
+        </small>
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
     );
   }
