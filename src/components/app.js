83299f917a3cbdc84628524a0b3995f888d4a9b3
Alex Cheng <alexcheng94@126.com>
2018-10-04 18:48:19 +0800

    fixed cache condition check

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Snackbar from "@material-ui/core/Snackbar";
 
-import Routes from './routes'
+import Routes from "./routes";
 
 export default class App extends Component {
   state = {
@@ -15,30 +15,46 @@ export default class App extends Component {
   componentDidMount() {
     const isOnline = window.navigator.onLine;
 
-    caches.has("posts").then(isCached => {
-      if (isCached && isOnline) {
-        setTimeout(() => {
-          this.setState({
-            showAlert: true,
-            alertMessage: "Posts cached for offline use",
-            alertDuration: 2000
-          });
-        }, 1500);
-      } else if (isCached && !isOnline) {
-        setTimeout(() => {
-          this.setState({
-            showAlert: true,
-            alertMessage: "You are in offline mode",
-            alertDuration: 2000
-          });
-        }, 1500);
-      }
-    });
+    // Check if api request to "https://alexsblogapi.herokuapp.com/posts" is cached:
+    caches.open("runtime-cache").then(cache =>
+      cache.match("https://alexsblogapi.herokuapp.com/posts").then(res => {
+        if (typeof res !== "undefined") {
+          // Is indeed cached
+          if (isOnline) {
+            //Alert user posts are cached
+            setTimeout(() => {
+              this.setState({
+                showAlert: true,
+                alertMessage: "Posts cached for offline use",
+                alertDuration: 2000
+              });
+            }, 1500);
+          } else {
+            // Alert user that they're in offline mode
+            setTimeout(() => {
+              this.setState({
+                showAlert: true,
+                alertMessage: "You are in offline mode",
+                alertDuration: 2000
+              });
+            }, 1500);
+          }
+        }
+      })
+    );
 
+    // Check the window.isUpdateAvailable promise that we defined in registerServiceWorker
+    // it resolves to true if a new version is available.
+
+    // Two reasons for the delay:
+    //  1. To allow the 'posts cached' alert to display first, without interruption;
+    //  2. There might be a race condition between this check and service worker registration,
+    //     otherwise window.isUpdateAvailable will be undefined
     setTimeout(() => {
       try {
         window.isUpdateAvailable.then(isAvailable => {
           if (isAvailable) {
+            // Alert user new version available
             this.setState({
               showAlert: true,
               alertMessage: "New version available, please refresh!",
@@ -47,7 +63,7 @@ export default class App extends Component {
           }
         });
       } catch (err) {
-        //ignore
+        //ignore errors
       }
     }, 4000);
   }
@@ -55,7 +71,6 @@ export default class App extends Component {
     this.setState({ showAlert: false });
   }
   render() {
-
     const { showAlert, alertMessage, alertDuration } = this.state;
     return (
       <div>
