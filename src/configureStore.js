05e73bc6e389abc8ea4e4f1cec9ae8dfae35c18a
Alex Cheng <alexcheng94@126.com>
2019-12-12 15:19:08 +0800

    🔃 removed redux logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
 import thunk from "redux-thunk";
-import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
 const configureStore = () => {
   const middlewares = [promiseMiddleware(), thunk];
-  if (process.env.NODE_ENV !== "production") {
-    middlewares.push(createLogger());
-  }
 
   //redux devtools config
   const composeEnhancers =
