05371711e7544cdeda2318e5876fa5e5a3b9b9f9
Alex Cheng <alexcheng94@126.com>
2021-10-08 19:24:07 +0800

    🎨 added basic overlay styles

--- src/components/SearchOverlay.tsx
+++ src/components/SearchOverlay.tsx
@@ -1,14 +1,51 @@
 import React from "react";
+import { Paper, makeStyles } from "@material-ui/core";
 
 type Props = {
   open: boolean;
 };
 
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: "100%",
+    height: "100vh",
+    zIndex: 2,
+    display: "flex",
+    flexDirection: "column"
+  },
+  toolbarSpacer: theme.mixins.toolbar,
+  content: {
+    width: "100%",
+    height: "100%",
+    padding: 24,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    backdropFilter: "blur(1.5px)" // Uber-cool blur effect
+  },
+  paper: {}
+}));
+
 const SearchOverlay: React.FC<Props> = ({ open }) => {
+  const classes = useStyles();
+
   if (!open) {
     return null;
   }
-  return <div>Something wicked this way comes</div>;
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.toolbarSpacer}></div>
+      <div className={classes.content}>
+        <Paper className={classes.paper} elevation={3}>
+          <div>By the pricking of my thumbs</div>
+          <div>Something wicked this way comes</div>
+        </Paper>
+      </div>
+    </div>
+  );
 };
 
 export default SearchOverlay;
