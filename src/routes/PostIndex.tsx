e18e7b631704f0c00a1509bf50bb0608a809a716
Alex Cheng <alexcheng94@126.com>
2021-10-28 16:11:52 +0800

    ♻️ removed sorting switch

--- src/routes/PostIndex.tsx
+++ src/routes/PostIndex.tsx
@@ -1,24 +1,13 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { makeStyles, Grid, Tooltip, Switch } from "@material-ui/core";
+import { Grid, Tooltip } from "@material-ui/core";
 import { ErrorAlert, Cards, CardPlaceholder, NewPostButton } from "@components";
 import checkIfExpired from "../middlewares/checkTokenExpired";
 import { GET_ALL_POSTS } from "../gqlDocuments";
 import { loadingVar } from "../cache";
 
-const useStyles = makeStyles(theme => ({
-  switch: {
-    width: "100%",
-    marginTop: "-12px",
-    marginBottom: "-12px",
-    fontSize: "0.6em"
-  }
-}));
-
 const PostIndex = () => {
-  const [orderChecked, setOrderChecked] = useState(false);
   const { loading, error, data } = useQuery(GET_ALL_POSTS);
-  const classes = useStyles();
 
   useEffect(() => {
     loadingVar(loading);
@@ -35,28 +24,14 @@ const PostIndex = () => {
     <Fragment>
       {error && <ErrorAlert error={error} />}
       <Grid container spacing={3}>
-        {/* Sorting switch */}
-        <div className={classes.switch}>
-          <Switch
-            checked={orderChecked}
-            onChange={e => {
-              setOrderChecked(e.target.checked);
-            }}
-            value="orderChecked"
-          />
-          Sorting by:{" "}
-          <strong>{orderChecked ? "latest" : "oldest"} first</strong>
-        </div>
-
         {
           //Show placeholders when loading
           loading ? (
             <CardPlaceholder />
           ) : (
-            <Cards posts={data.posts} latestFirst={orderChecked} />
+            <Cards posts={data.posts} latestFirst={false} />
           )
         }
-
         {/* Direct user to sign up page or if already signed in, write new page */}
         <Tooltip title="Write a story">
           <NewPostButton destination={writeButtonPath} />
