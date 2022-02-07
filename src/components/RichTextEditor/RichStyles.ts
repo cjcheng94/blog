3ce9ea7419d1997f96559dc432854ae3ecd314ab
Alex Cheng <alexcheng94@126.com>
2022-02-07 23:12:53 +0800

    💄 fixed block quote color for dark theme

--- src/components/RichTextEditor/RichStyles.ts
+++ src/components/RichTextEditor/RichStyles.ts
@@ -1,32 +1,35 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-  fileInput: {
-    display: "none"
-  },
-  editor: {
-    minHeight: 500,
-    cursor: "text",
-    fontSize: "1.2em",
-    marginTop: 10,
+const useStyles = makeStyles(theme => {
+  const isDarkTheme = theme.palette.type === "dark";
+  return {
+    fileInput: {
+      display: "none"
+    },
+    editor: {
+      minHeight: 500,
+      cursor: "text",
+      fontSize: "1.2em",
+      marginTop: 10,
 
-    "& .public-DraftEditorPlaceholder-root, & .public-DraftEditor-content": {
-      margin: "0 -15px -15px",
-      padding: 15
-    },
-    "& .public-DraftEditor-content": {
-      minHeight: 100
-    },
-    "& .RichEditor-blockquote": {
-      borderLeft: "5px solid #eee",
-      color: "#666",
-      fontFamily: '"Hoefler Text", "Georgia", serif',
-      fontStyle: "italic",
-      fontSize: "1.4em",
-      margin: "16px 0",
-      padding: "10px 20px"
+      "& .public-DraftEditorPlaceholder-root, & .public-DraftEditor-content": {
+        margin: "0 -15px -15px",
+        padding: 15
+      },
+      "& .public-DraftEditor-content": {
+        minHeight: 100
+      },
+      "& .RichEditor-blockquote": {
+        borderLeft: "5px solid #eee",
+        color: isDarkTheme ? "#ddd" : "#666",
+        fontFamily: '"Hoefler Text", "Georgia", serif',
+        fontStyle: "italic",
+        fontSize: "1.4em",
+        margin: "16px 0",
+        padding: "10px 20px"
+      }
     }
-  }
-}));
+  };
+});
 
 export default useStyles;
