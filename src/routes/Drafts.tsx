e76e2f7093ba270bf6aff1b210a5d9f4c23b17a9
Alex Cheng <alexcheng94@126.com>
2022-02-13 16:07:24 +0800

    ✨ added login promt to /drafts

--- src/routes/Drafts.tsx
+++ src/routes/Drafts.tsx
@@ -1,19 +1,41 @@
 import React, { Fragment, useEffect } from "react";
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { ErrorAlert, Cards, CardPlaceholder, NewPostButton } from "@components";
 import { GET_USER_DRAFTS } from "../api/gqlDocuments";
-import { loadingVar } from "../api/cache";
+import { loadingVar, accountDialogTypeVar } from "../api/cache";
+import checkIfExpired from "../utils/checkTokenExpired";
 import { Draft } from "PostTypes";
 
-const Drafts = () => {
-  const { loading, error, data } =
-    useQuery<{ getUserDrafts: Draft[] }>(GET_USER_DRAFTS);
+const showAccountDialog = (type: "login" | "signup") => {
+  accountDialogTypeVar(type);
+};
 
-  // TODO: Prompt user to log in
+const Drafts = () => {
+  const [getUserDrafts, { loading, error, data }] =
+    useLazyQuery<{ getUserDrafts: Draft[] }>(GET_USER_DRAFTS);
+
+  const isAuthenticated = !checkIfExpired();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      //Prompt user to log in
+      showAccountDialog("login");
+      return;
+    }
+    // If user has already logged in, get drafts
+    getUserDrafts();
+  }, []);
 
   useEffect(() => {
     loadingVar(loading);
   }, [loading]);
+
+  // Get drafts after user has logged in
+  useEffect(() => {
+    if (isAuthenticated) {
+      getUserDrafts();
+    }
+  }, [isAuthenticated]);
 
   const renderCards = () => {
     if (loading || !data?.getUserDrafts) {
