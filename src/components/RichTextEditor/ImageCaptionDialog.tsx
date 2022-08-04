0f68c5a14c628dc5db0e108b7daaf79fcff1f7f3
Alex Cheng <alexcheng94@126.com>
2022-08-04 18:45:25 +0800

    ✨ added captions

---
+++ src/components/RichTextEditor/ImageCaptionDialog.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  makeStyles
+} from "@material-ui/core";
+import { useMutation } from "@apollo/client";
+import { UPDATE_IMAGE } from "../../api/gqlDocuments";
+
+const useStyles = makeStyles(theme => ({
+  input: {
+    width: "100%"
+  },
+  errorMessage: {
+    width: "100%",
+    color: theme.palette.error.main
+  }
+}));
+
+type Props = {
+  imageId: string;
+  onClose: () => void;
+};
+// We'd have to deal w/ Draft entities and add unnecessary complexities if we were to put an input in MediaComponent,
+// Instead, we use a Dialog outside of the Draft editor, as the editor state should not care about the caption at all
+const ImageCaptionDialog: React.FC<Props> = props => {
+  const [value, setValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const classes = useStyles();
+
+  const [updateImage, { loading, error, data }] = useMutation(UPDATE_IMAGE);
+
+  const { onClose, imageId } = props;
+
+  const handleClose = () => {
+    setValue("");
+    onClose();
+  };
+
+  useEffect(() => {
+    if (!loading && data) {
+      handleClose();
+    }
+  }, [loading, data]);
+
+  const handleUpdate = () => {
+    if (value.length > 180) {
+      setErrorMessage("Caption text can't exceed 180 characters");
+      return;
+    }
+
+    setErrorMessage("");
+    updateImage({
+      variables: {
+        _id: imageId,
+        caption: value
+      }
+    });
+  };
+
+  return (
+    <Dialog open={true} onClose={handleClose}>
+      <DialogTitle>Edit Caption</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          className={classes.input}
+          value={value}
+          onChange={e => {
+            setValue(e.target.value);
+          }}
+        />
+        {errorMessage && (
+          <div className={classes.errorMessage}>{errorMessage}</div>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleUpdate} color="secondary" disabled={loading}>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ImageCaptionDialog;
